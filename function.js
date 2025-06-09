452ce949cd5c148b2d313dd308f6760bac8b9bdc
hjliss0628 <hunterliss99@gmail.com>
2025-06-09 15:31:25 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,12 @@
-function markConfirmed() {
-  const msgDiv = document.getElementById("message");
-  msgDiv.textContent = "Maildrop address confirmed. You’re good to go!";
-}
+Office.onReady(info => {
+  if (info.host === Office.HostType.Outlook) {
+    // Only show if we're in compose
+    if (Office.context.mailbox.item.itemType === Office.MailboxEnums.ItemType.Message) {
+      Office.context.mailbox.item.notificationMessages.addAsync("maildropReminder", {
+        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+        message: "Please add the matter's maildrop address or maildrop@polarislawyers.com as a recipient, CC, or BCC.",
+        persistent: true
+      });
+    }
+  }
+});
